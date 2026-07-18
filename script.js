1977b32a7ca1a7b7a13063775c75b1061860442b
eclipse1mpala <sunrisewarm.21@gmail.com>
2026-07-19 01:00:59 +0300

    Update Supabase client initialization and keys

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const SUPABASE_URL = "https://supabase.co";
-const SUPABASE_ANON_KEY = "СЮДА_ВСТАВЬТЕ_ВАШ_КЛЮЧ_sb_publishable";
+const SUPABASE_ANON_KEY = "sb_publishable_u75OjV44Wx5I1j_ErFBpvg_QLLWkLvp";
 
-const supabase = initialSupabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 async function loadMessages() {
     const { data, error } = await supabase
