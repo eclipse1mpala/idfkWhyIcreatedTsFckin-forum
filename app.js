bf7a6f539494ee57714df21a2dcc04f2d75039fb
eclipse1mpala <sunrisewarm.21@gmail.com>
2026-07-19 02:00:15 +0300

    Refactor message loading and sending functions

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const SUPABASE_URL = "https://supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_u75OjV44Wx5I1j_ErFBpvg_QLLWkLvp";
 
-// Официальный вызов библиотеки из источника unpkg
+
 const db = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 async function loadMessages() {
